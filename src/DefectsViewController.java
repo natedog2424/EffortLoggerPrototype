3640309e00fd1beab17ed678b4f18ac2374be2da
Evan Severtson <ejsevertson@gmail.com>
2023-04-15 15:15:17 -0700

    Pushing some more defect stuff for testing

--- src/DefectsViewController.java
+++ src/DefectsViewController.java
@@ -37,36 +37,115 @@ public class DefectsViewController {
 
 	private FXMLLoader fxmlLoader;
 
-	public DefectsViewController(ArrayList<Defect> UnresolvedDefectsInit , ArrayList<Defect> ResolvedDefectsInit){
-		this.UnresolvedDefectsList = UnresolvedDefectsInit;
-		this.ResolvedDefectsList = ResolvedDefectsInit;
-		DefectFXMLPane = new FXMLLoader(DefectsViewController.getResource("DefectPane.fxml"));
+	public DefectsViewController(){
+		this.UnresolvedDefectsList = new ArrayList<Defect>;
+		this.ResolvedDefectsList = new ArrayList<Defect>;
+		this.DefectFXMLPane = new FXMLLoader(DefectsViewController.getResource("DefectPane.fxml"));
 	}
 	
 	@FXML
 	protected void AddUnresolvedDefectEvent() {
 		Stage add = new Stage();
+		Label Type = new Label("Type");
+		TextField TypeField = new TextField(WantToEditDefect.DefectType);
+		Label Description = new Label("Description");
+		TextField DescriptionField = new TextField(WantToEditDefect.DefectDescription);
+		Button DoneAdding = new Button("Done");
+		DoneEditing.setOnAction(e->{
+			try{
+				String NewType = TypeField.getText();
+				String NewDescription = DescriptionField.getText();
+				if(NewType.isEmpty() || NewDescription.isEmpty()){
+					throw new Exception("At least one field is empty");
+				}
+				Defect NewDefect = new Defect(NewType ,NewDescription);
+				UnresolvedDefectsList.add(NewDefect);
+				defectDisplay.add(NewDefect.selected);
+			}
+			catch{
+				Stage error = new Stage();
+				Button closeButton = new Button("Close");
+				closeButton.setOnAction(e -> error.close());
+				Label errorMessage = new Label(exception.getMessage());
+				VBox errorHolder = new VBox(10,errorMessage,closeButton);
+				errorHolder.setAlignment(Pos.CENTER);
+				error.setTitle("Error Message");
+				error.setScene(new Scene(errorHolder, 400,100));
+				error.show();
+			}
+
+		}
+		);
 
 	}
 	@FXML
 	protected void EditUnresolvedDefectEvent() {
 		try{
 			Defect WantToEditDefect;
-			for(int i = 0; i < UnresolvedDefectsList.size()-1; i++){
-				if(UnresolvedDefectsList.get(i).selected && WantToEditDefect == null){
+			int DefectIndex = -1;
+			for(int i = 0; i < UnresolvedDefectsList.size(); i++){
+				if(UnresolvedDefectsList.get(i).selected && DefectIndex == -1){
 					WantToEditDefect = UnresolvedDefectsList.get(i);
-					continue;
+					DefectIndex = i;
 				}
-				else if(UnresolvedDefectsList.get(i).selected && WantToEditDefect != null){
-					throw new Exception("You can only edit one defect at a time!")
+				else if(UnresolvedDefectsList.get(i).selected && DefectIndex != -1){
+					throw new Exception("You can only edit one defect at a time!");
+					break;
 				}
 			}
-			if(WantToEditDefect == null){
+			if(DefectIndex == -1){
 				throw new Exception("Must select at least one defect to edit!")
 			}
 			else{
+				Stage Edit = new Stage();
+				Label Type = new Label("Type");
+				TextField TypeField = new TextField(WantToEditDefect.DefectType);
+				Label Description = new Label("Description");
+				TextField DescriptionField = new TextField(WantToEditDefect.DefectDescription);
+				Button DoneEditing = new Button("Done");
+				DoneEditing.setOnAction(e->{
+					try{
+						String NewType = TypeField.getText();
+						String NewDescription = DescriptionField.getText();
+						if(NewType.isEmpty() || NewDescription.isEmpty()){
+							throw new Exception("At least one field is empty");
+						}
+						Defect NewDefect = new Defect(NewType , NewDescription);
+						UnresolvedDefectsList.set(DefectIndex, NewDefect);
+						defectDisplay.clear();
+						for(int i = 0; i < UnresolvedDefectList.size(); i++){
+							defectDisplay.add(UnresolvedDefectList.get(i));
+						}
+					}
+					catch{
+						Stage error = new Stage();
+						Button closeButton = new Button("Close");
+						closeButton.setOnAction(e -> error.close());
+						Label errorMessage = new Label(exception.getMessage());
+						VBox errorHolder = new VBox(10,errorMessage,closeButton);
+						errorHolder.setAlignment(Pos.CENTER);
+						error.setTitle("Error Message");
+						error.setScene(new Scene(errorHolder, 400,100));
+						error.show();
+					}
 
+				}
+				);
+				Button CancelEditing = new Button("Cancel");
+				CancelEditing.setOnAction(e -> Edit.close());
+				
 			}
+		}
+		catch(Exception exception){
+			Stage error = new Stage();
+			Button closeButton = new Button("Close");
+			closeButton.setOnAction(e -> error.close());
+			Label errorMessage = new Label(exception.getMessage());
+			VBox errorHolder = new VBox(10,errorMessage,closeButton);
+			errorHolder.setAlignment(Pos.CENTER);
+			error.setTitle("Error Message");
+			error.setScene(new Scene(errorHolder, 400,100));
+			error.show();
 		}
 
 	}
