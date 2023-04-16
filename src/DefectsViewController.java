06303621fc2016d82fc38e9080800f1d76cc1966
Evan Severtson <ejsevertson@gmail.com>
2023-04-16 12:32:00 -0700

    Yep

--- src/DefectsViewController.java
+++ src/DefectsViewController.java
@@ -38,8 +38,8 @@ public class DefectsViewController {
 	private FXMLLoader fxmlLoader;
 
 	public DefectsViewController(){
-		this.UnresolvedDefectsList = new ArrayList<Defect>;
-		this.ResolvedDefectsList = new ArrayList<Defect>;
+		this.UnresolvedDefectsList = new ArrayList<Defect>();
+		this.ResolvedDefectsList = new ArrayList<Defect>();
 		this.DefectFXMLPane = new FXMLLoader(DefectsViewController.getResource("DefectPane.fxml"));
 	}
 	
@@ -52,7 +52,6 @@ public class DefectsViewController {
 		TextField DescriptionField = new TextField(WantToEditDefect.DefectDescription);
 		Button DoneAdding = new Button("Done");
 		DoneEditing.setOnAction(e->{
-			try{
 				String NewType = TypeField.getText();
 				String NewDescription = DescriptionField.getText();
 				if(NewType.isEmpty() || NewDescription.isEmpty()){
@@ -61,19 +60,6 @@ public class DefectsViewController {
 				Defect NewDefect = new Defect(NewType ,NewDescription);
 				UnresolvedDefectsList.add(NewDefect);
 				defectDisplay.add(NewDefect.selected);
-			}
-			catch{
-				Stage error = new Stage();
-				Button closeButton = new Button("Close");
-				closeButton.setOnAction(e -> error.close());
-				Label errorMessage = new Label(exception.getMessage());
-				VBox errorHolder = new VBox(10,errorMessage,closeButton);
-				errorHolder.setAlignment(Pos.CENTER);
-				error.setTitle("Error Message");
-				error.setScene(new Scene(errorHolder, 400,100));
-				error.show();
-			}
-
 		}
 		Button CancelAdd = new Button("Cancel");
 		CancelAdd.setOnAction(e -> Edit.close());
@@ -84,14 +70,14 @@ public class DefectsViewController {
 	@FXML
 	protected void EditUnresolvedDefectEvent() {
 		try{
-			Defect WantToEditDefect;
+			Defect WantToEditDefect = new Defect();
 			int DefectIndex = -1;
 			for(int i = 0; i < UnresolvedDefectsList.size(); i++){
-				if(UnresolvedDefectsList.get(i).selected && DefectIndex == -1){
+				if(UnresolvedDefectsList.get(i).SelectedDefect && DefectIndex == -1){
 					WantToEditDefect = UnresolvedDefectsList.get(i);
 					DefectIndex = i;
 				}
-				else if(UnresolvedDefectsList.get(i).selected && DefectIndex != -1){
+				else if(UnresolvedDefectsList.get(i).SelectedDefect && DefectIndex != -1){
 					throw new Exception("You can only edit one defect at a time!");
 					break;
 				}
@@ -101,15 +87,15 @@ public class DefectsViewController {
 			}
 			else{
 				Stage Edit = new Stage();
+				GridPane EditPane = new GridPane();
 				Label Type = new Label("Type");
 				TextField TypeField = new TextField(WantToEditDefect.DefectType);
 				Label Description = new Label("Description");
 				TextField DescriptionField = new TextField(WantToEditDefect.DefectDescription);
 				Button DoneEditing = new Button("Done");
 				DoneEditing.setOnAction(e->{
-					try{
-						String NewType = TypeField.getText();
-						String NewDescription = DescriptionField.getText();
+						String NewType = TypeField.getText().trim();
+						String NewDescription = DescriptionField.getText().trim();
 						if(NewType.isEmpty() || NewDescription.isEmpty()){
 							throw new Exception("At least one field is empty");
 						}
@@ -117,27 +103,26 @@ public class DefectsViewController {
 						UnresolvedDefectsList.set(DefectIndex, NewDefect);
 						defectDisplay.clear();
 						for(int i = 0; i < UnresolvedDefectList.size(); i++){
-							defectDisplay.add(UnresolvedDefectList.get(i));
+							defectDisplay.add(UnresolvedDefectList.get(i).selected);
 						}
-					
-					}
-					catch{
-						Stage error = new Stage();
-						Button closeButton = new Button("Close");
-						closeButton.setOnAction(e -> error.close());
-						Label errorMessage = new Label(exception.getMessage());
-						VBox errorHolder = new VBox(10,errorMessage,closeButton);
-						errorHolder.setAlignment(Pos.CENTER);
-						error.setTitle("Error Message");
-						error.setScene(new Scene(errorHolder, 400,100));
-						error.show();
-					}
 
 				}
 				);
 				Button CancelEditing = new Button("Cancel");
 				CancelEditing.setOnAction(e -> Edit.close());
-				
+				Hbox Buttons = new Hbox(10, DoneEditing, CancelEditing);
+				EditPane.setHgap(10);
+				EditPane.setVgap(10);
+				EditPane.setPadding(new Insets(20));
+				EditPane.add(Type, 0 ,0);
+				EditPane.add(TypeField, 1 ,0);
+				EditPane.add(Description,  0,1);
+				EditPane.add(DescriptionField, 1 ,1);
+				Vbox EditDisplayCombined = new Vbox(10,EditPane,Buttons);
+				Edit.setScene(new Scene(EditDisplayCombined, 400, 300));
+				Edit.setTitle("Edit Defect");
+        		Edit.show();
+
 			}
 		}
 		catch(Exception exception){
