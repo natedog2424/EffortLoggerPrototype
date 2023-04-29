a344cd144ecea038d217d61bc95aef6bd2d0b6dd
Evan Severtson <ejsevertson@gmail.com>
2023-04-29 15:42:13 -0700

    gjhsad

--- src/Defect.java
+++ src/Defect.java
@@ -9,15 +9,18 @@ public class Defect {
 
 	public int EffortLevel;
 
-	public String Combined;
 
 	public Defect(String Type, String Description, String EffortLevel){
 		this.DefectDescription = Description;
 		this.DefectType = Type;
 		this.EffortLevel = Integer.parseInt(EffortLevel);
-		this.Combined = DefectType + "\t\t\t\t" + DefectDescription + "\t\t\t\tEstimate Effort Level: "+ EffortLevel;
+		
 	
 
+	}
+
+	public String defectToString(){
+		return DefectType + "\t\t\t\t" + DefectDescription + "\t\t\t\tEstimated Effort Level: "+ EffortLevel;
 	}
 
 
