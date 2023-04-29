3c17f83da4e9fbdf51d9d855e2ac226f68914f87
Evan Severtson <ejsevertson@gmail.com>
2023-04-29 13:16:30 -0700

    YepCock

--- src/MainViewController.java
+++ src/MainViewController.java
@@ -44,6 +44,7 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TabPane;
@@ -81,7 +82,7 @@ public class MainViewController implements Initializable {
 	private ToggleGroup group;
 
 	@FXML
-	private Button roleButton;
+	public Button roleButton;
 
 	private BooleanProperty logsPaneSelected;
 
@@ -89,9 +90,9 @@ public class MainViewController implements Initializable {
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		URI audioFileURI = new File("resources/EffortLogger_soundEffect.mp3").toURI();
-		Media audioMedia = new Media(audioFileURI.toString());
-		mediaPlayer = new MediaPlayer(audioMedia);
+		//URI audioFileURI = new File("resources/EffortLogger_soundEffect.mp3").toURI();
+		//Media audioMedia = new Media(audioFileURI.toString());
+		//mediaPlayer = new MediaPlayer(audioMedia);
 		
 		//load all tabs
 		for (int i = 0; i < TabPanes.length; i++) {
@@ -102,7 +103,7 @@ public class MainViewController implements Initializable {
 				if (i == 2) {
 					LogsViewController controller = TabLoader.getController();
 					logsPaneSelected = controller.tabSelectedProperty();
-					mediaPlayer.play();
+					//mediaPlayer.play();
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -139,6 +140,56 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	protected void roleButtonPressed(){
+		Stage RoleEdit = new Stage();
+		RoleEdit.setTitle("Edit Role");
+		GridPane RolePane = new GridPane();
+		TextField RoleField;
+		
+		Label Role = new Label("Role");
+		if(roleButton.getText().equals("No Role")){
+			RoleField = new TextField();
+		}
+		else{
+			RoleField = new TextField(roleButton.getText());
+		}
+		Button DoneEditing = new Button("Done");
+		DoneEditing.setOnAction(e->{
+						try{
+			
+						String NewRole = RoleField.getText().trim();
+						if(NewRole.isEmpty()){
+							throw new Exception("Please enter a role");
+						}
+						roleButton.setText(NewRole);
+						RoleEdit.close();
+						}
+						catch(Exception exception){
+							Stage error = new Stage();
+							Button closeButton = new Button("Close");
+							closeButton.setOnAction(errorMes -> error.close());
+							Label errorMessage = new Label(exception.getMessage());
+							VBox errorHolder = new VBox(10,errorMessage,closeButton);
+							errorHolder.setAlignment(Pos.CENTER);
+							error.setTitle("Error Message");
+							error.setScene(new Scene(errorHolder, 400,100));
+							error.show();
+						}
+				}
+				);
+		Button CancelEditing = new Button("Cancel");
+		CancelEditing.setOnAction(e -> RoleEdit.close());
+		HBox Buttons = new HBox(10, DoneEditing, CancelEditing);
+		Buttons.setAlignment(Pos.CENTER);
+		RolePane.setAlignment(Pos.CENTER);
+		RolePane.setHgap(30);
+		RolePane.setVgap(10);
+		RolePane.setPadding(new Insets(20));
+		RolePane.add(Role, 0 ,0);
+		RolePane.add(RoleField, 1 ,0);
+		VBox RoleEditDisplayCombined = new VBox(10,RolePane,Buttons);
+		RoleEdit.setScene(new Scene(RoleEditDisplayCombined, 300, 150));
+				
+        RoleEdit.show();	
 		
 	}
 
