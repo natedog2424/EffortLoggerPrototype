2e68d6e0b2c1be55d85326eb21f66fc775ed505b
natedog2424 <natedog2424mail@gmail.com>
2023-04-30 20:15:09 -0700

    run scripts

---
+++ runMacM1.sh
@@ -0,0 +1,1 @@
+java -cp ./bin --module-path './lib/javafx-sdk-20/lib' --add-modules javafx.controls,javafx.fxml,javafx.media App
---
+++ runWindows.bat
@@ -0,0 +1,1 @@
+java -cp ./bin --module-path './lib/javafx-sdk-20-win/lib' --add-modules javafx.controls,javafx.fxml,javafx.media App
