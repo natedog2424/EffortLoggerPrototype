25f5d947459a69539e05f5da6e8c2ca3de26ed54
natedog2424 <natedog2424mail@gmail.com>
2023-04-30 19:56:03 -0700

    i may have brain damage

--- src/StringUtilities.java
+++ src/StringUtilities.java
@@ -1,4 +1,4 @@
-public class Util {
+public class StringUtilities {
     public static String capitalizeString(String str) {
 		return str.substring(0, 1).toUpperCase() + str.substring(1).toLowerCase();
 	}
